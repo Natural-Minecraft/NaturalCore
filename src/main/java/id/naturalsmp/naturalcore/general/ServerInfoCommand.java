d9f94f3959fafa90cf8e4c7169d33def013773d1
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-01-28 05:54:16 +0700

    fix: add missing List import in ServerInfoCommand

--- src/main/java/id/naturalsmp/naturalcore/general/ServerInfoCommand.java
+++ src/main/java/id/naturalsmp/naturalcore/general/ServerInfoCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
