bafee5c208200113f7d5ae5e3213d92eebffed7c
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-01-28 06:47:17 +0700

    fix: resolve repeatable annotation error in ImageUtils

--- src/main/java/id/naturalsmp/naturalcore/banner/ImageUtils.java
+++ src/main/java/id/naturalsmp/naturalcore/banner/ImageUtils.java
@@ -53,7 +53,6 @@ public class ImageUtils {
      * This is CRITICAL for performance.
      */
     @SuppressWarnings("deprecation")
-    @SuppressWarnings("deprecation")
     public static byte[] convertToMapColors(BufferedImage part) {
         byte[] colors = new byte[128 * 128];
         Map<Integer, Byte> matchCache = new HashMap<>();
